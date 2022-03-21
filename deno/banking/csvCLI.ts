b992d5257770e7896d10c8a8a1fc9c4a3666d66f
HT <hntran98@gmail.com>
2022-03-21 01:08:51 -0400

    Support from/to date range filtering

--- deno/banking/csvCLI.ts
+++ deno/banking/csvCLI.ts
@@ -1,8 +1,24 @@
 import { parse as parseArgs } from "https://deno.land/std@0.128.0/flags/mod.ts"
-import { checkFiles, categorizeFiles } from "./csvCategorize.ts"
+import { parseDate, checkFiles, categorizeFiles } from "./csvCategorize.ts"
 
 function checkUsage(args: any) {
   if (args["_"].length > 0) {
+    return false;
+  }
+
+  try {
+    if (args.from) {
+      args.from = parseDate(args.from);
+      //console.debug(args.from);
+    }
+    if (args.to) {
+      args.to = parseDate(args.to);
+      //console.debug(args.to);
+    }
+  } catch (err) {
+    if (args.debug) {
+      console.log(err);
+    }
     return false;
   }
   return true;
@@ -15,7 +31,7 @@ const args = parseArgs(Deno.args, {
     d: "."
   },
   boolean: ["a", "stats", "debug"],
-  string: ["f", "d"],
+  string: ["f", "d", "from", "to"],
   alias: {
     f: "file",
     d: "dir",
@@ -32,12 +48,15 @@ if (args.help || !checkUsage(args)) {
   console.log(`Parse banking CSV files and summarize the transactions based on "Description 1" and "Description 2" columns in CAD$\n
 Examples:
   csvCLI.exe -d c:\\laundromat\\bank -f "2020 - banking.csv" -f "2021 - banking.csv"
+  csvCLI.exe -d c:\\laundromat\\bank --stats -o --from 1-1-2022 --to 12-31-2022 -s
   csvCLI.exe -d c:\\laundromat\\bank --summary
 
 Usage:
   -f or --file <CSV file> : specify the path to CSV file (repeat option to specify multiple files)
   -d or --dir <source dir> : specify the folder containing CSV file(s) to parse (defaults to current directory)
-  -s or --summary: print transaction summary for each CSV file as well as grand totals
+  -s or --summary : print transaction summary for each CSV file as well as grand totals
+  --from <date> : specify minimum date to filter out transactions (dd-mm-yyyy)
+  --to <date> : specify maximum date to filter out transactions (dd-mm-yyyy)
   --debug : print diagnostics for validating the processing
   -h or --help : show this usage help`);
 } else {
