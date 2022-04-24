dbb1a16078a9807a83c5b55210b0ef17ba53c3e9
HT <hntran98@gmail.com>
2022-04-23 23:29:23 -0400

    Add API_URL, API_KEY to run script

--- deno/roi/deno_run.bat
+++ deno/roi/deno_run.bat
@@ -1,2 +1,4 @@
 @echo off
+set API_URL=http://localhost/cashflow
+set API_KEY=CHANGE ME!!
 deno run --allow-env --allow-net --allow-run --watch index.ts
--- deno/roi/index.ts
+++ deno/roi/index.ts
@@ -521,7 +521,7 @@ const html = `
           <div class="accordion-item">
             <h2 class="accordion-header" id="header-income">
               <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-income" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                Income
+                Income / Revenue
               </button>
             </h2>
             <div id="collapse-income" class="accordion-collapse collapse show" aria-labelledby="header-income">
