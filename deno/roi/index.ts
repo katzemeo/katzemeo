a81f334542d2c80f706ccf598637372e17642608
HT <hntran98@gmail.com>
2022-05-07 11:51:15 -0400

    Show error / alerts messages

--- deno/roi/index.ts
+++ deno/roi/index.ts
@@ -47,6 +47,19 @@ const html = `
   </head>
   <body oncontextmenu="return false;">
   <div class="container mt-3">
+    <div id="header">
+      <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+        <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+        </symbol>
+        <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+        </symbol>
+        <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </symbol>
+      </svg>
+    </div>
     <ul class="m-0 nav nav-fill nav-justified nav-tabs" id="tab-nav" role="tablist">
       <li class="nav-item" role="presentation" title="Introduction and Notes"> <button class="active nav-link" name="home" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-pane" type="button" role="tab" aria-controls="home-pane" aria-selected="true"><nobr><i class="fas fa-home"></i> Home</button></nobr></li>
       <li class="nav-item" role="presentation" title="Cash Count for Date"> <button class="nav-link" name="cashcount" id="cashcount-tab" data-bs-toggle="tab" data-bs-target="#cashcount-pane" type="button" role="tab" aria-controls="cashcount-pane" aria-selected="false"><nobr><i class="fas fa-coins"></i> Cash Count</button></nobr></li>
@@ -330,10 +343,15 @@ const html = `
     const _percentFormat = new Intl.NumberFormat("en-US", { minimumFractionDigits: 0, maximumFactionDigits: 2 }).format;
     const PCT = function (value) { if (!isNaN(value)) { return _percentFormat(value); } return ""; };
     const CUR = new Intl.NumberFormat("en-US", { currency: "USD", style: "currency", currencyDisplay: "symbol", currencySign: "accounting" }).format;
+    const TIME_ZONE = Intl.DateTimeFormat().resolvedOptions().timeZone ?? "America/Toronto";
     var _guid = null;
     var _profile = null;
     var _cash_count_template = null;
     var _cash_flow_template = null;
+
+    const formatDate = (dt, timeZone = TIME_ZONE) => {
+      return dt ? new Date(dt).toLocaleDateString('en-us', { timeZone: timeZone, weekday: "long", year: "numeric", month: "short", day: "numeric" }) : "";
+    }
 
     const removeChildren = (parent, header = 0) => {
       while (parent.lastChild && parent.childElementCount > header) {
@@ -352,9 +370,11 @@ const html = `
       return defaultValue;
     };
 
-    const createDiv = (className, title=null) => {
+    const createDiv = (className=null, title=null) => {
       const el = document.createElement("div");
-      el.className = className;
+      if (className) {
+        el.className = className;
+      }
       if (title) {
         el.title = title;
       }
@@ -488,6 +508,49 @@ const html = `
       return divRowEntry;
     }
 
+    const showAlert = (type, icon, message) => {
+      const parentEl = document.getElementById("header");
+      const divAlert = createDiv("alert "+ type +" d-flex align-items-center alert-dismissible fade show");
+      divAlert.role = "alert";
+
+      if (icon) {
+        //const svg = document.createElement("svg");
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        //svg.className = "bi flex-shrink-0 me-2";
+        //svg.width = "24";
+        //svg.height = "24";
+        //svg.role = "img";
+        svg.setAttribute("class", "bi flex-shrink-0 me-2");
+        svg.setAttribute("width", "24");
+        svg.setAttribute("height", "24");
+        svg.setAttribute("role", "img");
+        svg.innerHTML = '<use href="#' + icon + '"/>';
+        divAlert.appendChild(svg);  
+      }
+
+      const divMessage = createDiv();
+      divMessage.innerText = message;
+      divAlert.appendChild(divMessage);
+      const button = document.createElement("button");
+      button.className = "btn-close";
+      button.setAttribute("data-bs-dismiss", "alert");
+      button.setAttribute("aria-label", "Close");
+      divAlert.appendChild(button);
+      parentEl.appendChild(divAlert);
+    };
+
+    const showInfo = (message) => {
+      showAlert("alert-primary", "info-fill", message);
+    };
+
+    const showWarning = (message) => {
+      showAlert("alert-warning", "exclamation-triangle-fill", message);
+    };
+
+    const showError = (message) => {
+      showAlert("alert-danger", "exclamation-triangle-fill", message);
+    };
+
     const buildCashCountUI = () => {
       const parentEl = document.getElementById("accordion-items-denominations");
       _cash_count_template.denominations.forEach((group) => {
@@ -575,6 +638,7 @@ const html = `
             buildCashCountUI();
           });
         } else {
+          showError("Unable to lookup Cash Count template.");
           console.log("Unexpected response", res.status);
         }
       }).catch((error) => {
@@ -609,6 +673,7 @@ const html = `
             buildCashFlowUI();
           });
         } else {
+          showError("Unable to lookup Cash Flow template.");
           console.log("Unexpected response", res.status);
         }
       }).catch((error) => {
@@ -677,6 +742,8 @@ const html = `
           tabEl.click();
         }
       }
+
+      showInfo("Today is "+ formatDate(nowDate));
     };
 
     function setTextColour(el, value, className = null) {
@@ -829,6 +896,7 @@ const html = `
           el.value = PCT(cagr*100) +" %";
           if (cagr < 0) {
             el.className = "form-control text-danger";
+            showWarning("Compound Annual Growth Rate is Negative!");
           } else {
             el.className = "form-control text-success";
           }
