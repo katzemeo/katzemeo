a61f53d81d791ec3ef8bcdc81601e9bf527c82d5
hntran98 <hntran98@users.noreply.github.com>
2024-10-14 01:51:18 -0400

    Stream back large files

--- website/index.tsx
+++ website/index.tsx
@@ -47,6 +47,27 @@ const html = `
   </body>
 </html>`
 
+async function createReadableStream(fileName) {
+  const body = new ReadableStream({
+    type: "bytes",
+    async start(controller) {
+      const file = await Deno.open(fileName, {read: true});
+      let readBlockSize=100000;
+      while (true) {
+        const buf = new Uint8Array(readBlockSize);
+        await Deno.seek(file.rid, readBlockSize, Deno.SeekMode.Current)
+        const numberOfBytesRead = await Deno.read(file.rid, buf);
+        if (!numberOfBytesRead) {
+          controller.close();
+          break;
+        }
+        controller.enqueue(buf);
+      }
+    },
+  });
+  return body;
+}
+
 async function handleRequest(request: Request): Promise<Response> {
   const { pathname } = new URL(request.url);
 
@@ -68,8 +89,11 @@ async function handleRequest(request: Request): Promise<Response> {
     });
   } else if (pathname.startsWith("/dnf4life.")) {
     //console.debug(`${pathname}`);
-    const file = await Deno.readFile("."+ pathname);
-    return new Response(file, {
+
+    // Note: reading entire into memory fails on Deno Deploy due to 512MB max memory is available!
+    // https://docs.deno.com/subhosting/manual/pricing_and_limits/#:~:text=512MB%20max%20memory%20is%20available.
+    //const file = await Deno.readFile("."+ pathname);
+    return new Response(createReadableStream("."+ pathname), {
       headers: {
         "Cache-Control": "public, max-age=31536000",
       },
